f448b3a793293555ac56cdf7ac834b3afe378d8f
bitbytejoy <bitbytejoy@gmail.com>
2018-12-13 10:18:26 +0100

    Favicon.

--- components/Layout.js
+++ components/Layout.js
@@ -11,8 +11,9 @@ export default ({ title, children, openGraphUrl }) => (
       <meta property="og:title" content={title}/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content={openGraphUrl}/>
-      <meta property="og:image" content={imageUrl("myself.jpg")}/>
+      <meta property="og:image" content={imageUrl('myself.jpg')}/>
       <title>{title}</title>
+      <link rel="shortcut icon" type="image/png" href={imageUrl('favicon.png')}/>
       <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Extended|Monoton|Roboto:400,700,900" rel="stylesheet"/>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js"/>
     </Head>
