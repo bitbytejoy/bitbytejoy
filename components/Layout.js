e55d61837ce85cf68301a990d777e2edf5932724
bitbytejoy <bitbytejoy@gmail.com>
2018-12-13 04:10:16 +0100

    OG Fix URL.

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Footer from './Footer'
 import LanguagePicker from './LanguagePicker'
-import appUrl from '../util/appUrl'
+import imageUrl from '../util/imageUrl'
 import style from '../style/style'
 
 export default ({ title, children, openGraphUrl }) => (
@@ -11,7 +11,7 @@ export default ({ title, children, openGraphUrl }) => (
       <meta property="og:title" content={title}/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content={openGraphUrl}/>
-      <meta property="og:image" content={appUrl("/static/myself.jpg")}/>
+      <meta property="og:image" content={imageUrl("myself.jpg")}/>
       <title>{title}</title>
       <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Extended|Monoton|Roboto:400,700,900" rel="stylesheet"/>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js"/>
